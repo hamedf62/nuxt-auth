341c9af5edd91faae6854a904c3a1580fd34b9b2
Hamed Fallah <hamedf@gmail.com>
2024-01-17 20:07:01 +0330

    new changes

--- src/runtime/composables/local/useAuth.ts
+++ src/runtime/composables/local/useAuth.ts
@@ -22,12 +22,12 @@ type Credentials = {
   password?: string;
 } & Record<string, any>;
 
-const signIn = async (credentials, signInOptions, signInParams) => {
+const signIn = async (credentials: string | Credentials | URLSearchParams | Record<string, string> | string[][] | undefined, signInOptions: SecondarySignInOptions | undefined, signInParams?: undefined) => {
   const nuxt = useNuxtApp()
   const config = useTypedBackendConfig(useRuntimeConfig(), 'local')
   const { path, method, headers } = config.endpoints.signIn
 
-  const response = await _fetch(nuxt, path, {
+  const response: Record<string, any> = await _fetch(nuxt, path, {
     method,
 
     body: new URLSearchParams({
